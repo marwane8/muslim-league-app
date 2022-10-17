c9e3208d163afd2a8535ef9212afd4ce52980fbb
Marwane <taroua.marwane@gmail.com>
2022-10-17 18:04:03 -0400

    frontend - set up testing

--- ml-ui/components/Navbar.tsx
+++ ml-ui/components/Navbar.tsx
@@ -17,7 +17,7 @@ const Navbar = () => (
                 </Link>
             </li>
             <li>
-                <Link href="/test">
+                <Link href="/example">
                     Teams 
                 </Link>
             </li>
