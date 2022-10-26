1f6a5043765945b4af9c2bc797362072e6d67854
Marwane <taroua.marwane@gmail.com>
2022-10-25 23:42:26 -0400

    CHECKPOINT 2: Minimum Useable App with semi clean code

--- ml-api/app/main.py
+++ ml-api/app/main.py
@@ -6,12 +6,11 @@ from fastapi.responses import JSONResponse
 from app.models import Player,Team, User, UserJSON
 from app.utils import ( 
     create_access_token,
-    get_hashed_password,
     verify_password,
     get_user_from_db 
 )
 
-from app.db_access import (
+from app.db_accessor import (
     create_standings,
     get_team_roster
 )
@@ -34,17 +33,6 @@ app.add_middleware(
 @app.get("/")
 def home():
    return  { "message": "Muslim League API"}
-
-#TODO: Fix SignUp Page
-@app.post("/signup", summary="Create new user", response_model=User)
-def signup(user: User):
-    
-    hashpass = get_hashed_password(user.password)
-    user = {
-        'username': user.username,
-        'password': hashpass
-    }     
-    return user
 
 
 @app.post("/login",summary="Create access and refresh tokens for user", response_model=UserJSON)
@@ -77,11 +65,6 @@ def logout(user: User = Depends(get_current_user)):
     response.set_cookie(key="token", value="",secure=True)
     return response
 
-@app.get("/me", summary='Get details of currently logged in user', response_model=User)
-def get_me(user: User = Depends(get_current_user)):
-    return user
-
-
 @app.get("/standings/{season_id}" , response_model=list[Team])
 def get_roster(season_id: int = Path(None,description="The ID of a Season")):
     standings = create_standings(season_id)
@@ -91,34 +74,3 @@ def get_roster(season_id: int = Path(None,description="The ID of a Season")):
 def get_roster(team_id: int = Path(None,description="The ID of a Team")):
     roster = get_team_roster(team_id)
     return roster 
-
-player = {
-    "id":1,
-    "name":"Mike",
-    "pos":"C"
-
-}
-players = {}
-players[1] = player
-
-@app.get("/get-player/{player_id}")
-def get_player(player_id: int = Path(None,description="The ID of a player")):
-    return players[player_id] 
-
-#Example Query Parameter
-@app.get("/get-by-name")
-def get_player_name(name: str | None = None):
-    for player_id in players:
-        print(player_id)
-        if players[player_id]["name"] == name:
-            return players[player_id]
-    return {"Data" : "Not Found"}
-
-
-#Post method
-@app.post("/create-player/{player_id}")
-def create_player(player_id: int, player: Player):
-    if player_id in players:
-        return {"Error": "Item ID already exsists."}
-    players[player_id] = player
-    return players[player_id]
